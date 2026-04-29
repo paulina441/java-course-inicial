88f5419437a4f1ab8f80ea94c296433e51570b58
paulina441 <castanoarenasmariapaulina@gmail.com>
2026-04-29 14:38:21 -0500

    OK DOM JS

--- trabajo-final-js/js/app.js
+++ trabajo-final-js/js/app.js
@@ -1,6 +1,25 @@
 let boton = document.getElementById("consultarBtn");
 let contenedor = document.getElementById("resultados");
 
+async function cargarDatos() {
+    contenedor.innerHTML = "";
+
+    const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+    const datos = await respuesta.json();
+
+    datos.forEach(usuario => {
+        const tarjeta = document.createElement("div");
+        tarjeta.classList.add("tarjeta");
+
+        tarjeta.innerHTML = `
+            <h3>${usuario.name}</h3>
+            <p>${usuario.email}</p>
+        `;
+
+        contenedor.appendChild(tarjeta);
+    });
+}
+
 boton.addEventListener("click", () => {
-    console.log("Botón funcionando");
+    cargarDatos();
 });
