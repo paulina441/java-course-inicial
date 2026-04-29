071f5902efab223cca17be38ccc98f92223da0e5
paulina441 <castanoarenasmariapaulina@gmail.com>
2026-04-29 14:22:14 -0500

    OK HTML and CSS

---
+++ trabajo-final-js/js/app.js
@@ -0,0 +1,6 @@
+let boton = document.getElementById("consultarBtn");
+let contenedor = document.getElementById("resultados");
+
+boton.addEventListener("click", () => {
+    console.log("Botón funcionando");
+});
